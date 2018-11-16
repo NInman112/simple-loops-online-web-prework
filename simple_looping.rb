8e37a965c95f449e555a25794ee87f1b0c777c2e
Nick Inman <nick@theoatmeal.com>
2018-11-16 01:45:18 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,7 +40,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times = 0
   
-  while number_of_times <= 7
+  while number_of_times < 7
     puts phrase
     number_of_times += 1
   end
